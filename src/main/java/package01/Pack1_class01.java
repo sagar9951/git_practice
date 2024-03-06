5ef4d6213b4540c2f3b55ba808e823d611f7aef2
SagarK98 <skarmakar1861995@gmail.com>
2024-03-06 17:11:17 +0530

    class 03 created

--- src/main/java/package01/Pack1_class01.java
+++ src/main/java/package01/Pack1_class01.java
@@ -1,5 +1,9 @@
 package package01;
 
+import org.testng.annotations.Test;
+
 public class Pack1_class01 {
 
+	@Test
+	void testscript01() {}
 }
---
+++ src/test/java/package3/Class_03.java
@@ -0,0 +1,9 @@
+package package3;
+
+import org.testng.annotations.Test;
+
+public class Class_03 {
+
+	@Test
+	void tesctcase_03() {}
+}
